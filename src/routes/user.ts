3b585f47d498f2fffdbe95e3af250d16feb5a445
Rafael Mata <matarafael2004@gmail.com>
2024-10-13 16:35:42 -0400

    refactored auth routes

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,15 +1,18 @@
 import { User } from "../models/user";
 import express from 'express'
+import { auth } from '../services/auth'
+import { resetRequestSchema } from "../schemas/auth";
 
 const router = express.Router();
 export default router;
 
-router.get("/:username", async (req, res) => {
-    const { username } = req.params;
-    const requestedUser = await User.findOne({username});
-    res.json(requestedUser);
+router.delete("/:username", async (req, res) => {
+    
 })
 
-router.delete("/:username", async (req, res) => {
-    console.log(req.params, req.body)
+router.put('/:username/change-email', (req, res) => {
+    const { username } = req.params;
+    const body = resetRequestSchema.safeParse(req.body);
+
+    // if (!body.su)
 })
--- src/services/user.ts
+++ src/services/user.ts
@@ -1,9 +1,17 @@
 import { User } from "../models/user"
 
-const changeUserEmail = async (user_id: string, newEmail: string) => {
-    const foundUser = await User.findOne({ _id: user_id });
-    if (!foundUser) throw new Error('User does not exist');
+const changeUserEmail = async (_id: string, newEmail: string) => {
+    const foundUser = await User.findOne({ _id });
+    if (!foundUser || foundUser.deleted) throw new Error('User does not exist');
 
     foundUser.email = newEmail;
+    await foundUser.save();
+}
+
+const deleteUser = async (_id: string) => {
+    const foundUser = await User.findOne({ _id });
+    if (!foundUser || foundUser.deleted) throw new Error('User does not exist');
+
+    foundUser.deleted = true;
     await foundUser.save();
 }
