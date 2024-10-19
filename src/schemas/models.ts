f193d3200ee124a5d50a0eafdf320e667a580d8e
Rafael Mata <matarafael2004@gmail.com>
2024-10-18 23:48:54 -0400

    As you know, madness is like gravity: All it takes is a little push.

--- src/schemas/models.ts
+++ src/schemas/models.ts
@@ -2,7 +2,7 @@ import { z } from 'zod'
 
 export const noteSchema = z.object({
     title: z.string().max(30),
-    content: z.string().max(250),
+    content: z.string(),
     categories: z.array(z.string()),
     priority: z.number().min(0).max(5),
     favorite: z.boolean()
