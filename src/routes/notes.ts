a09b7b175375179009e413039534bbb1a810039b
Rafael Mata <matarafael2004@gmail.com>
2024-10-20 20:03:38 -0400

    fix: several notes actually can have the same title, I don't like it but ok

--- src/routes/notes.ts
+++ src/routes/notes.ts
@@ -26,25 +26,6 @@ router.get('/:username/notes', auth, async (req, res) => {
     }
 });
 
-// router.get('/:username/notes/:title', auth, async (req, res) => {
-//     try {
-//         const { username, title } = req.params;
-//         const foundUser = await User.findOne({ $and: [{ username }, {deleted: false}]});
-//         if (!foundUser) 
-//             throw new Error('User not found');
-
-//         const foundNote = await Note.findOne(
-//             { owner: foundUser._id }, {deleted: false}, {title}
-//         );
-
-//         res.status(200).json({ success:"Found note!", data: foundNote});
-//     }
-//     catch (error) {
-//         console.error(error);
-//         res.status(500).json(getErrorMessage(error));
-//     }
-// })
-
 router.get('/:username/notes/:category', auth, async (req, res) => {
     try {
         const { username, category } = req.params;
@@ -115,9 +96,6 @@ router.post('/:username/note', auth, async (req, res) => {
             throw new Error('User not found');
         
         const {title, content, priority, favorite, categories} = body.data;
-        const foundNote = await Note.findOne({ $and: [{ owner: foundUser._id }, { deleted: false }, { title }]})
-        if (foundNote) 
-            throw new Error('Title already used');
         
         const newNote = new Note({
             title, content, priority, favorite, categories, owner: foundUser._id
