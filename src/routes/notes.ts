1d568812df4f764591a8d0482953742c53d3b8aa
Rafael Mata <matarafael2004@gmail.com>
2024-10-23 18:28:51 -0400

    I'm not political. I'm just trying to make people laugh.

--- src/routes/notes.ts
+++ src/routes/notes.ts
@@ -143,8 +143,8 @@ router.put('/:username/note/:_id', auth, async (req, res) => {
             console.log(userCategories);
 
             (propsToChange.categories as string[]).forEach((category) => {
-                console.log(`${category} in ${userCategories}: ${category in userCategories}`)
-                if (!(category in userCategories))
+                console.log(`${category} in [${userCategories}]: ${userCategories.includes(category)}`)
+                if (!userCategories.includes(category))
                     throw new Error('Categories not found');
             });
         }
